f7397068d3ba2bd53a9fc38a6d556a0bd1160754
Lendic42 <ivanterentev834@gmail.com>
2026-07-18 21:02:42 +0300

    chore: bundle Unity Capture Uninstall-LumaStage-Camera.bat

---
+++ apps/desktop/resources/unity-capture/Uninstall-LumaStage-Camera.bat
@@ -0,0 +1,14 @@
+@echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+  echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\lumastage_uc_unadmin.vbs"
+  echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %*", "", "runas", 1 >> "%temp%\lumastage_uc_unadmin.vbs"
+  "%temp%\lumastage_uc_unadmin.vbs"
+  del "%temp%\lumastage_uc_unadmin.vbs"
+  exit /B
+)
+cd /D "%~dp0"
+regsvr32 /u /s "UnityCaptureFilter32.dll"
+regsvr32 /u /s "UnityCaptureFilter64.dll"
+echo Uninstalled.
+exit /B 0
